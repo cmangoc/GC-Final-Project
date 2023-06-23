55c2dca62cf8d552214d9d7f32801118897e0e51
daniellarsen77 <daniellarsen127@gmail.com>
2023-06-23 10:46:43 -0400

    Updated gitignore

--- src/app/golf-details.service.ts
+++ src/app/golf-details.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GolfDetailsService {
-  private readonly API_URL = 'https://localhost:5001/api/GolfCourses';
+  private readonly API_URL = 'https://golf-course-finder.p.rapidapi.com/course/details';
+  private readonly RAPIDAPI_HOST = 'golf-course-finder.p.rapidapi.com';
+  private readonly RAPIDAPI_KEY = '166c2d797fmsh28c82b65e687bdap1c7882jsn9874022a5a9b';
 
   constructor(private http: HttpClient) { }
 
   getCourseDetails(zip: string, name: string): Observable<any> {
-    const url = `${this.API_URL}?zip=${zip}&name=${name}`;
-
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'X-RapidAPI-Host': this.RAPIDAPI_HOST,
+      'X-RapidAPI-Key': this.RAPIDAPI_KEY
     });
 
-    return this.http.get(url, { headers });
+    const params = new HttpParams()
+      .set('zip', zip)
+      .set('name', name);
+
+    return this.http.get(this.API_URL, { headers, params });
   }
 }
